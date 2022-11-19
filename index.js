a95445c302918e788ce768cb6dfdbf53c681b9b0
Austen Cloud <AustenCloud@gmail.com>
2022-11-19 10:51:06 -0600

    removed the last edit

--- index.js
+++ index.js
@@ -9,45 +9,37 @@ function addOneDrop(){
     console.log(number1)
     screen1El.innerText = number1
 }
-window.addOneDrop = addOneDrop
 function addTwoDrops(){
     number1 += 2
     console.log(number1)
     screen1El.innerText = number1
 }
-window.addTwoDrops = addTwoDrops
 function addThreeDrops(){
     number1 += 3
     console.log(number1)
     screen1El.innerText = number1
 }
-window.addThreeDrops = addThreeDrops
 function resetDrops(){
     screen1El.textContent = "0"
     number1 = 0
 }
-window.resetDrops = resetDrops
 // CATCHES //
 function addOneCatch(){
     number2 += 1
     console.log(number2)
     screen2El.innerText = number2
 }
-window.addOneCatch = addOneCatch
 function addTwoCatches(){
     number2 += 2
     console.log(number2)
     screen2El.innerText = number2
 }
-window.addTwoCatches = addTwoCatches
 function addThreeCatches(){
     number2 += 3
     console.log(number2)
     screen2El.innerText = number2
 }
-window.addThreeCatches = addThreeCatches
 function resetCatches(){
     screen2El.textContent = "0"
     number2 = 0
 }
-window.resetCatches = resetCatches
