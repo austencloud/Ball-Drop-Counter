1e01d27ae9acc11a1024093a55e53569f7ac7222
austencloud <68725581+austencloud@users.noreply.github.com>
2022-11-19 13:02:34 -0600

    Update index.js

--- index.js
+++ index.js
@@ -6,17 +6,16 @@ let screen2El = document.getElementById("screen2")
 // DROPS //
 function addOneDrop(){
     number1 += 1
-    console.log(number1)
     screen1El.innerText = number1
 }
+window.addOneDrop = addOneDrop
+
 function addTwoDrops(){
     number1 += 2
-    console.log(number1)
     screen1El.innerText = number1
 }
 function addThreeDrops(){
     number1 += 3
-    console.log(number1)
     screen1El.innerText = number1
 }
 function resetDrops(){
@@ -26,17 +25,14 @@ function resetDrops(){
 // CATCHES //
 function addOneCatch(){
     number2 += 1
-    console.log(number2)
     screen2El.innerText = number2
 }
 function addTwoCatches(){
     number2 += 2
-    console.log(number2)
     screen2El.innerText = number2
 }
 function addThreeCatches(){
     number2 += 3
-    console.log(number2)
     screen2El.innerText = number2
 }
 function resetCatches(){
