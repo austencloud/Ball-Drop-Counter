ae47970a31540586109ac1340eb6e319e072a73d
austencloud <68725581+austencloud@users.noreply.github.com>
2022-11-19 13:08:26 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,54 +2,46 @@ let number1 = 0
 let number2 = 0
 let screen1El = document.getElementById("screen1")
 let screen2El = document.getElementById("screen2")
+window.addOneDrop = addOneDrop
+window.addTwoDrops = addTwoDrops
+window.addThreeDrops = addThreeDrops
+window.resetDrops = resetDrops
+window.addOneCatch = addOneCatch
+window.addTwoCatches = addTwoCatches
+window.addThreeCatches = addThreeCatches
+window.resetCatches = resetCatches
 
 // DROPS //
 function addOneDrop(){
     number1 += 1
     screen1El.innerText = number1
 }
-window.addOneDrop = addOneDrop
-
 function addTwoDrops(){
     number1 += 2
     screen1El.innerText = number1
 }
-window.addTwoDrops = addTwoDrops
-
 function addThreeDrops(){
     number1 += 3
     screen1El.innerText = number1
 }
-window.addThreeDrops = addThreeDrops
-
 function resetDrops(){
     screen1El.textContent = "0"
     number1 = 0
 }
-window.resetDrops = resetDrops
-
-
 // CATCHES //
 function addOneCatch(){
     number2 += 1
     screen2El.innerText = number2
 }
-window.addOneCatch = addOneCatch
-
 function addTwoCatches(){
     number2 += 2
     screen2El.innerText = number2
 }
-window.addTwoCatches = addTwoCatches
-
 function addThreeCatches(){
     number2 += 3
     screen2El.innerText = number2
 }
-window.addThreeCatches = addThreeCatches
-
 function resetCatches(){
     screen2El.textContent = "0"
     number2 = 0
 }
-window.resetCatches = resetCatches
